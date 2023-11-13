ac718c7419e5e8f0e0baf685fda8e98e500a9b77
LAPTOP-UIQ3JNM8\guenh <guenhiep@gmail.com>
2023-11-14 00:20:07 +0700

    first commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,195 @@
+import 'package:app_fwithsqlite01/db_helper.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Map<String, dynamic>> _allData = [];
+
+  bool _isLoading = true;
+  // Get All Data From Database
+  void _refreshData() async {
+    await SQLHelper.db();
+    final data = await SQLHelper.getAllData();
+    print('run at here $data');
+    setState(() {
+      _allData = data;
+      _isLoading = false;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _refreshData();
+  }
+
+  
+
+// Add Data
+  Future<void> _addData() async {
+    await SQLHelper.createdData(_titleController.text, _descController.text);
+    _refreshData();
+  }
+
+// Update Data
+  Future<void> _updateData(int id) async {
+    await SQLHelper.updateData(id, _titleController.text, _descController.text);
+    _refreshData();
+  }
+
+// Delete Data
+  void _deleteData(int id) async {
+    await SQLHelper.deleteData(id);
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      backgroundColor: Colors.redAccent,
+      content: Text("Data Deleted"),
+    ));
+    _refreshData();
+  }
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descController = TextEditingController();
+
+  void showBottomSheet(int? id) async {
+    if (id != null) {
+      final existingData =
+          _allData.firstWhere((element) => element['id'] == id);
+      _titleController.text = existingData['title'];
+      _descController.text = existingData['desc'];
+    }
+
+    showModalBottomSheet(
+      elevation: 5,
+      isScrollControlled: true,
+      context: context,
+      builder: (_) => Container(
+        padding: EdgeInsets.only(
+          top: 30,
+          left: 15,
+          right: 15,
+          bottom: MediaQuery.of(context).viewInsets.bottom + 50,
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Title",
+              ),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              controller: _descController,
+              maxLines: 4,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Description",
+              ),
+            ),
+            SizedBox(height: 20),
+            Center(
+              child: ElevatedButton(
+                onPressed: () async {
+                  if (id == null) {
+                    await _addData();
+                  }
+                  if (id != null) {
+                    await _updateData(id);
+                  }
+
+                  _titleController.text = "";
+                  _descController.text = "";
+
+                  Navigator.of(context).pop();
+                  print("Data Added");
+                },
+                child: Padding(
+                  padding: EdgeInsets.all(18),
+                  child: Text(
+                    id == null ? "Add Data" : "Update ",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String ImgLink =
+        "https://wallpaperaccess.in/public/uploads/preview/anime-chainsaw-man-power-chainsaw-man-hd-wallpaper-q.jpg";
+    return Scaffold(
+      backgroundColor: Colors.pink[200],
+      appBar: AppBar(
+        title: Text("Hiep to do List"),
+      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(ImgLink), fit: BoxFit.contain),
+              ),
+              child: ListView.builder(
+                itemCount: _allData.length,
+                itemBuilder: (context, index) => Card(
+                  margin: EdgeInsets.all(15),
+                  child: ListTile(
+                    title: Padding(
+                      padding: EdgeInsets.symmetric(vertical: 5),
+                      child: Text(
+                        _allData[index]['title'],
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                    subtitle: Text(_allData[index]['desc']),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            showBottomSheet(_allData[index]['id']);
+                          }, 
+                          icon: Icon(
+                            Icons.edit,
+                            color: Colors.indigo[300],
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            _deleteData(_allData[index]['id']);
+                          }, 
+                          icon: Icon(
+                            Icons.delete,
+                            color: Colors.black54,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => showBottomSheet(null),
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:app_fwithsqlite01/home_screen.dart';
+import 'package:flutter/material.dart';
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // sqfliteFfiInit();
+  // databaseFactory = databaseFactoryFfi;
+  runApp(const MyApp());
+}
+// Thử chạy lại tteen đt xem khi chiều t chạy đc mà
+// nó cx lỗi như này
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
